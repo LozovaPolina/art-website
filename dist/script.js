45d35b7a601426b231d32e9150e24b70c54bbcc0
Polina Lozova <1polinalozovaya1@gmail.com>
2024-01-06 17:00:58 +0300

    add sliders script

--- dist/script.js
+++ dist/script.js
@@ -100,12 +100,80 @@ const modals = () => {
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
   bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
   showModalByScroll('.fixed-gift');
-  // bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
-  // bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
   showModalByTime('.popup-consultation', 5000);
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
+
+/***/ }),
+
+/***/ "./src/js/modules/sliders.js":
+/*!***********************************!*\
+  !*** ./src/js/modules/sliders.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const sliders = (slides, dir, prev, next, time) => {
+  let slideIndex = 1,
+    paused = false;
+  const items = document.querySelectorAll(slides);
+  function showSlides(n) {
+    if (n > items.length) {
+      slideIndex = 1;
+    } else if (n < 1) {
+      slideIndex = items.length;
+    }
+    items.forEach(item => {
+      item.classList.add('animated');
+      item.style.display = 'none';
+    });
+    items[slideIndex - 1].style.display = 'block';
+  }
+  showSlides(slideIndex);
+  function changeSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  try {
+    const prevBtn = document.querySelector(prev),
+      nextBtn = document.querySelector(next);
+    prevBtn.addEventListener('click', e => {
+      changeSlides(-1);
+      items[slideIndex - 1].classList.remove('slideInLeft');
+      items[slideIndex - 1].classList.add('slideInRight');
+    });
+    nextBtn.addEventListener('click', e => {
+      changeSlides(1);
+      items[slideIndex - 1].classList.remove('slideInRight');
+      items[slideIndex - 1].classList.add('slideInLeft');
+    });
+  } catch (e) {}
+  function activateAnimation() {
+    if (dir === 'vertical') {
+      paused = setInterval(() => {
+        changeSlides(1);
+        items[slideIndex - 1].classList.add('slideInDown');
+      }, time);
+    } else {
+      paused = setInterval(() => {
+        changeSlides(1);
+        items[slideIndex - 1].classList.remove('slideInRight');
+        items[slideIndex - 1].classList.add('slideInLeft');
+      }, time);
+    }
+  }
+  activateAnimation();
+  items[0].parentNode.addEventListener('mouseenter', () => {
+    clearInterval(paused);
+  });
+  items[0].parentNode.addEventListener('mouseleave', () => {
+    activateAnimation();
+  });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (sliders);
 
 /***/ })
 
@@ -173,11 +241,15 @@ var __webpack_exports__ = {};
   \************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_madals__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/madals */ "./src/js/modules/madals.js");
+/* harmony import */ var _modules_sliders__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/sliders */ "./src/js/modules/sliders.js");
+
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   (0,_modules_madals__WEBPACK_IMPORTED_MODULE_0__["default"])();
+  (0,_modules_sliders__WEBPACK_IMPORTED_MODULE_1__["default"])('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn', 3000);
+  (0,_modules_sliders__WEBPACK_IMPORTED_MODULE_1__["default"])('.main-slider-item', 'vertical', '', '', 6000);
 });
 })();
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from "./modules/madals";
+import sliders from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
-    modals()
+    modals();
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn', 3000);
+    sliders('.main-slider-item', 'vertical', '', '', 6000);
 })
